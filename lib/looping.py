1e1a5adda23a4dc83bab6dd96a721759e025de74
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-11-12 09:11:53 +0300

    done

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+        print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list = [int ** 2 for int in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    #check the most specific condition first
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
